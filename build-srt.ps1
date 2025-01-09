08de8576ab14892b1639ed16356b0734183ee10d
雷鳴 <sorayuki@live.com>
2025-01-09 12:13:49 +0800

    enable parallel build

--- build-srt.ps1
+++ build-srt.ps1
@@ -62,7 +62,7 @@ function CloneGitLatestRelease {
 
     if ([Directory]::Exists($dir) -eq $false) {
         $release_tag = (GetGitLatestRelease -repo $repo)
-        Start-Process -WorkingDirectory "." -Wait -NoNewWindow -FilePath "git.exe" -ArgumentList ("clone", "--depth=1", "--recursive", "-b", $release_tag, $repo, $dir)
+        Start-Process -WorkingDirectory "." -Wait -NoNewWindow -FilePath "git.exe" -ArgumentList ("clone", "--recurse-submodules", "--depth=1", "-b", $release_tag, $repo, $dir)
     }
 }
 
@@ -124,7 +124,7 @@ foreach($is_static_crt in $static_crt_list) {
             Start-Process -WorkingDirectory "./mbedtls" -Wait -NoNewWindow -FilePath "git.exe" -ArgumentList ("clean", "-xfd")
             Start-Process -WorkingDirectory "./mbedtls" -Wait -NoNewWindow -FilePath "git.exe" -ArgumentList ("checkout", ".")
             [File]::WriteAllLines([string]"mbedtls/build.bat", [string[]]("cmake -S . -B " + "build" + " -A " + $bits + " -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_MSVC_RUNTIME_LIBRARY=`"MultiThreaded$<$<CONFIG:Debug>:Debug>" + $crt_link + "`" -DCMAKE_BUILD_TYPE=" + $buildtype + " -DCMAKE_INSTALL_PREFIX=" + $instdir + " -DENABLE_PROGRAMS=OFF -DENABLE_TESTING=OFF"))
-            [File]::AppendAllLines([string]"mbedtls/build.bat", [string[]]("cmake --build build --config " + $buildtype))
+            [File]::AppendAllLines([string]"mbedtls/build.bat", [string[]]("cmake --build build --config " + $buildtype + " -- -m"))
             [File]::AppendAllLines([string]"mbedtls/build.bat", [string[]]("cmake --install build --config " + $buildtype))
             Start-Process -WorkingDirectory "./mbedtls" -Wait -NoNewWindow -FilePath "cmd.exe" -ArgumentList ("/c", "build.bat")
 
@@ -134,7 +134,7 @@ foreach($is_static_crt in $static_crt_list) {
             $extra_param = $extra_param + " -DENABLE_STDCXX_SYNC=ON"
             
             [File]::WriteAllLines([string]"srt/build.bat", [string[]]("cmake -S . -B " + "build -A " + $bits + " -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_MSVC_RUNTIME_LIBRARY=`"MultiThreaded$<$<CONFIG:Debug>:Debug>" + $crt_link + "`" -DCMAKE_BUILD_TYPE=" + $buildtype + " -DCMAKE_INSTALL_PREFIX=" + $instdir + " -DENABLE_APPS=ON -DENABLE_SHARED=ON -DENABLE_STATIC=ON -DUSE_ENCLIB=mbedtls" + $extra_param))
-            [File]::AppendAllLines([string]"srt/build.bat", [string[]]("cmake --build build --config " + $buildtype))
+            [File]::AppendAllLines([string]"srt/build.bat", [string[]]("cmake --build build --config " + $buildtype + " -- -m"))
             [File]::AppendAllLines([string]"srt/build.bat", [string[]]("cmake --install build --config " + $buildtype))
             Start-Process -WorkingDirectory "./srt" -Wait -NoNewWindow -FilePath "cmd.exe" -ArgumentList ("/c", "build.bat")
         }
